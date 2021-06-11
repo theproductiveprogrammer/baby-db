81ae9dd809f4181c14686748bfbf99deb5294b0d
Charles L <charles.lobo@gmail.com>
2021-06-12 08:59:47 +1000

    feat: add test case

---
+++ example/example.js
@@ -0,0 +1,37 @@
+'use strict'
+const nodedb = require('..')
+const path = require('path')
+
+const USERS = {}
+
+const userdb = nodedb(path.join(__dirname, 'users.db'))
+
+let numerrs = 0
+userdb.on('error', err => {
+  numerrs++
+  console.error(err)
+})
+
+userdb.on('rec', (rec, num) => {
+  switch(rec.type) {
+    case 'new':
+      USERS[rec.userid] = rec.info
+      break
+    case 'update':
+      if(!USERS[rec.userid]) throw `Cannot update non-existent record on line: ${num}`
+      Object.assign(USERS[rec.userid], rec.info)
+      break
+    case 'delete':
+      delete USERS[rec.userid]
+      break
+    default:
+      throw `Did not understand record type: "${rec.type}", on line: ${num}`
+  }
+})
+userdb.on('done', () => {
+  console.log()
+  console.log('******')
+  console.log('Users loaded', JSON.stringify(USERS, 0, 2))
+  if(numerrs) console.log(`FOUND ${numerrs} errors!`)
+  console.log("ready to rumble....!")
+})
